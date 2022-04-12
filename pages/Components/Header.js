a1540c71e04cf316e106859367f3fc760628741d
Salih Simsek <salihsimseek@gmail.com>
2022-04-12 23:36:28 +0300

    signIn functionality added

--- pages/Components/Header.js
+++ pages/Components/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+import { signIn } from "../../near/config";
+
 const Header = () => {
   return (
     <div className="h-16 w-screen border-b-2 border-red-100 flex items-center justify-center">
       <div className="container flex items-center justify-between">
         <span className="text-xl cursor-default select-none">Todo App</span>
         {/* <span className='text-xl cursor-default select-none'>Near Protocol</span> */}
-        <button className="border-2 rounded-md border-black px-4 py-1 hover:bg-black hover:text-white hover:shadow-2xl hover:shadow-red-900">
+        <button
+          onClick={signIn}
+          className="border-2 rounded-md border-black px-4 py-1 hover:bg-black hover:text-white hover:shadow-2xl hover:shadow-red-900"
+        >
           Sign In
         </button>
       </div>
